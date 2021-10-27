710af4826082b67c3526a8e0930a11442076e204
Erasmo Jr <erasmo.cjunior@gmail.com>
2021-10-27 16:51:22 -0300

    Saving

---
+++ src/main/java/academy/devdojo/springboot/service/AnimeService.java
@@ -0,0 +1,37 @@
+package academy.devdojo.springboot.service;
+
+import academy.devdojo.springboot.domain.Anime;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
+
+@Service
+public class AnimeService {
+    private static List<Anime> animes;
+    static {
+        animes = new ArrayList<>(List.of(new Anime(1L, "Fate Stay Night"), new Anime(2L, "The Rising of Shield Hero")));
+    }
+    // private final AnimeRepository animeRepository;
+
+    public List<Anime> listAll() {
+        return animes;
+    }
+
+    public Anime findById(long id) {
+        return animes.stream()
+                .filter(anime -> anime.getId().equals(id))
+                .findFirst()
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Anime not found"));
+    }
+
+    public Anime save(Anime anime) {
+        anime.setId(ThreadLocalRandom.current().nextLong(3, 1000000));
+        animes.add(anime);
+        return anime;
+
+    }
+}
